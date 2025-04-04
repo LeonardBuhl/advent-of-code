cb07f1925a573d53c4c8058675d66fe2110ce05b
Leonard Buhl <leonard.buhl@telekom.de>
2025-04-04 20:21:24 +0200

    solved cc first try

--- src/2023/02/cube_conundrum_01_test.py
+++ src/2023/02/cube_conundrum_01_test.py
@@ -1,11 +1,24 @@
 import pytest
 from cube_conundrum_01 import Solution
 
-dataset = []
+dataset = [
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+]
+
 
 @pytest.fixture
 def solution():
     return Solution()
 
+
 def test_solution(solution: Solution):
-    assert solution.pass == 0
+    assert solution.find_possible_games(solution.prepare_data([dataset[0]])) == 1
+    assert solution.find_possible_games(solution.prepare_data([dataset[1]])) == 2
+    assert solution.find_possible_games(solution.prepare_data([dataset[2]])) == 0
+    assert solution.find_possible_games(solution.prepare_data([dataset[3]])) == 0
+    assert solution.find_possible_games(solution.prepare_data([dataset[4]])) == 5
+    assert solution.find_possible_games(solution.prepare_data(dataset)) == 8
---
+++ src/2023/02/cube_conundrum_02.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+from typing import List, Dict
+
+
+FILE_PATH = f"{Path(__file__).parent}/data.txt"
+
+# Structure:
+# { id : { color : amount , color: amount ... } }
+
+
+class Solution:
+    def read_file_in(self) -> List[str]:
+        dataset = []
+
+        with open(FILE_PATH, "r", encoding="UTF-8") as file:
+            for line in file:
+                dataset.append(line)
+
+        return dataset
+
+    def prepare_data(self, dataset: List[str]) -> Dict[int, Dict[str, int]]:
+        def build_color_dict(color_list: List[str]) -> Dict[str, int]:
+            color_dict = {}
+
+            color_dict["red"] = max(
+                [int(red.split()[0]) for red in color_list if "red" in red]
+            )
+            color_dict["green"] = max(
+                [int(green.split()[0]) for green in color_list if "green" in green]
+            )
+            color_dict["blue"] = max(
+                [int(blue.split()[0]) for blue in color_list if "blue" in blue]
+            )
+
+            return color_dict
+
+        structured_data = {}
+
+        for line in dataset:
+            key = line.split(":")[0].split()[1]
+            temp_value = line.split(":")[1]
+            temp_value = temp_value.split(";")
+            value = []
+            for lst in temp_value:
+                value.extend(lst.split(","))
+            structured_data[key] = build_color_dict(color_list=value)
+
+        return structured_data
+
+    def find_possible_games(self, structured_data: Dict[int, Dict[str, int]]) -> int:
+        set_power = 0
+        result = 0
+
+        for key, value in structured_data.items():
+            set_power = value["red"] * value["green"] * value["blue"]
+            result += set_power
+
+        return result
+
+
+def main():
+    sol = Solution()
+    raw_data = sol.read_file_in()
+    structured_data = sol.prepare_data(raw_data)
+    print(f"{sol.find_possible_games(structured_data)=}")
+
+
+if __name__ == "__main__":
+    main()
